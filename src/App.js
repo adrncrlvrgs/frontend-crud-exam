9b87e02fd3e9bf9faa69a3fa1e373718e9a6b8ae
jamieeedj <james.dejesus@qualitytrade.com>
2023-02-14 09:12:46 +0800

    fe crud exam boilerplate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import 'styles/main.scss';
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {Page} from 'components/Pages';
+import Home from 'views/home';
+import Users from 'views/users';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='app'>
+			<Router>
+				<Page>
+					<Switch>
+						<Route exact path='/'>
+							<Home />
+						</Route>
+						<Route path='/users'>
+							<Users />
+						</Route>
+						<Route path='*'>404</Route>
+					</Switch>
+				</Page>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
